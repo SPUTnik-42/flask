295c43082267f2b53257bbec6a56985c6e8f52d4
SPUTnik-42 <ushniknath@gmail.com>
2020-05-05 17:12:16 +0530

    LECTURE 2 FLASK BASICS [NOTE TAKER]

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import datetime 
-from flask import Flask, render_template
+from flask import Flask, render_template,request, session
+from flask_session import Session 
 
-app = Flask(__name__)
+app = Flask("__name__")
+
+
+notes = []
 
 @app.route('/')
 def index():
@@ -10,14 +14,37 @@ def index():
     return render_template('index.html',new_year=new_year)
 
 
+@app.route('/name')
+def names():
+    names = ['ushnik','utpal','mousumi','ushniha']
+    return render_template('index.html', names = names )
+
 @app.route('/bye')
 def bye():
     headline ="goodbye"
     return render_template('index.html', headline= headline)
-@app.route('/<string:name>')
-def hello(name):
-    name = name.capitalize()
-    return f"hello {name}"
+
+@app.route('/more')
+def more():
+    return render_template('more.html')
+
+@app.route('/hello', methods=["GET","POST"] )
+def hello():
+    if request.method == "GET" :
+        return "PLEASE SUBMIT THE FORM FROM /more"
+    else:   
+        name = request.form.get("name")
+        return render_template('hello.html', name=name )
+
+@app.route('/notes', methods =['GET','POST'])
+def mnotes(): 
+    if request.method == 'POST':
+        note = request.form.get('note')
+        notes.append(note)
+    return render_template('monotes.html',notes = notes)
+
+
+
 
 
 if __name__ == "__main__":
